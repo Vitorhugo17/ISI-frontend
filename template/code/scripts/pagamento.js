d1694af1a2d12dce1ef19417002bf33474465022
Vitorhugo17 <martinsvitor11@gmail.com>
2020-03-29 18:25:28 +0100

    alterações

--- template/code/scripts/pagamento.js
+++ template/code/scripts/pagamento.js
@@ -1,29 +1,35 @@
 // A reference to Stripe.js
+var urlBase = `http://localhost:8080`;
 var stripe;
 
 var orderData = {
-  items: [{ id: "photo-subscription" }],
-  currency: "usd"
+  product_id: "49245902",
+  quantity: "2",
+  company: "Barquense"
 };
 
 // Disable the button until we have Stripe set up on the page
 document.querySelector("button").disabled = true;
 
-fetch("/stripe-key").then(function(result) {
-    return result.json();
-  }).then(function(data) {
-    return setupElements(data);
-  }).then(function({ stripe, card, clientSecret }) {
-    document.querySelector("button").disabled = false;
+fetch(`${urlBase}/stripe-key`).then(function (result) {
+  return result.json();
+}).then(function (data) {  
+  return setupElements(data);
+}).then(function ({
+  stripe,
+  card,
+  clientSecret
+}) {
+  document.querySelector("button").disabled = false;
 
-    var form = document.getElementById("payment-form");
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      pay(stripe, card, clientSecret);
-    });
+  var form = document.getElementById("payment-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    pay(stripe, card, clientSecret);
   });
+});
 
-var setupElements = function(data) {
+var setupElements = function (data) {
   stripe = Stripe(data.publishableKey);
   /* ------- Set up Stripe Elements to use in checkout form ------- */
   var elements = stripe.elements();
@@ -43,7 +49,9 @@ var setupElements = function(data) {
     }
   };
 
-  var card = elements.create("card", { style: style });
+  var card = elements.create("card", {
+    style: style
+  });
   card.mount("#card-element");
 
   return {
@@ -53,12 +61,12 @@ var setupElements = function(data) {
   };
 };
 
-var handleAction = function(clientSecret) {
-  stripe.handleCardAction(clientSecret).then(function(data) {
+var handleAction = function (clientSecret) {
+  stripe.handleCardAction(clientSecret).then(function (data) {
     if (data.error) {
       showError("Your card was not authenticated, please try again");
     } else if (data.paymentIntent.status === "requires_confirmation") {
-      fetch("/pay", {
+      fetch(`${urlBase}/pay`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -66,15 +74,15 @@ var handleAction = function(clientSecret) {
         body: JSON.stringify({
           paymentIntentId: data.paymentIntent.id
         })
-      }).then(function(result) {
-          return result.json();
-        }).then(function(json) {
-          if (json.error) {
-            showError(json.error);
-          } else {
-            orderComplete(clientSecret);
-          }
-        });
+      }).then(function (result) {
+        return result.json();
+      }).then(function (json) {
+        if (json.error) {
+          showError(json.error);
+        } else {
+          orderComplete(clientSecret);
+        }
+      });
     }
   });
 };
@@ -82,43 +90,43 @@ var handleAction = function(clientSecret) {
 /*
  * Collect card details and pay for the order
  */
-var pay = function(stripe, card) {
+var pay = function (stripe, card) {
   changeLoadingState(true);
 
   // Collects card details and creates a PaymentMethod
-  stripe.createPaymentMethod("card", card).then(function(result) {
-      if (result.error) {
-        showError(result.error.message);
-      } else {
-        orderData.paymentMethodId = result.paymentMethod.id;
+  stripe.createPaymentMethod("card", card).then(function (result) {
+    if (result.error) {
+      showError(result.error.message);
+    } else {
+      orderData.paymentMethodId = result.paymentMethod.id;
 
-        return fetch("/pay", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(orderData)
-        });
-      }
-    }).then(function(result) {
-      return result.json();
-    }).then(function(response) {
-      if (response.error) {
-        showError(response.error);
-      } else if (response.requiresAction) {
-        // Request authentication
-        handleAction(response.clientSecret);
-      } else {
-        orderComplete(response.clientSecret);
-      }
-    });
+      return fetch(`${urlBase}/pay`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(orderData)
+      });
+    }
+  }).then(function (result) {
+    return result.json();
+  }).then(function (response) {
+    if (response.error) {
+      showError(response.error);
+    } else if (response.requiresAction) {
+      // Request authentication
+      handleAction(response.clientSecret);
+    } else {
+      orderComplete(response.clientSecret);
+    }
+  });
 };
 
 /* ------- Post-payment helpers ------- */
 
 /* Shows a success / error message when the payment is complete */
-var orderComplete = function(clientSecret) {
-  stripe.retrievePaymentIntent(clientSecret).then(function(result) {
+var orderComplete = function (clientSecret) {
+  stripe.retrievePaymentIntent(clientSecret).then(function (result) {
     var paymentIntent = result.paymentIntent;
     var paymentIntentJson = JSON.stringify(paymentIntent, null, 2);
 
@@ -126,7 +134,7 @@ var orderComplete = function(clientSecret) {
     document.querySelector("pre").textContent = paymentIntentJson;
 
     document.querySelector(".sr-result").classList.remove("hidden");
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector(".sr-result").classList.add("expand");
     }, 200);
 
@@ -134,17 +142,17 @@ var orderComplete = function(clientSecret) {
   });
 };
 
-var showError = function(errorMsgText) {
+var showError = function (errorMsgText) {
   changeLoadingState(false);
   var errorMsg = document.querySelector(".sr-field-error");
   errorMsg.textContent = errorMsgText;
-  setTimeout(function() {
+  setTimeout(function () {
     errorMsg.textContent = "";
   }, 4000);
 };
 
 // Show a spinner on payment submission
-var changeLoadingState = function(isLoading) {
+var changeLoadingState = function (isLoading) {
   if (isLoading) {
     document.querySelector("button").disabled = true;
     document.querySelector("#spinner").classList.remove("hidden");
