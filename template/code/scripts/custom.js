71044d7eb11c896d98b53eda65a7350241039a78
Ana Xavier Fernandes <ana.xavier.fernandes@gmail.com>
2020-03-30 21:13:26 +0100

    bla bla

--- template/code/scripts/custom.js
+++ template/code/scripts/custom.js
@@ -54,15 +54,6 @@ $(document).ready(function() {
 		]
 
 	}
-
-	window.onload = function(){
-		var pie_chart_1 = document.getElementById("generate-pie-chart").getContext("2d");
-		window.pie_chart_1 = new Chart(pie_chart_1).Pie(pieData);
-    
-		var bar_chart_1 = document.getElementById("generate-bar-chart").getContext("2d");
-		window.pie_chart_1 = new Chart(bar_chart_1).Bar(barChartData);
-		
-	};
                 
     //Share Bottom Socials
 
