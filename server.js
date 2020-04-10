d2542aa2641ff0aa875fc7fad194876a41440406
Vitorhugo17 <martinsvitor11@gmail.com>
2020-04-10 14:36:34 +0100

    alterações

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const validator = require('express-validator');
 const sanitizer = require('express-sanitizer');
 
-
 app.use(bodyParser.json({
     limit: '50mb'
 }), bodyParser.urlencoded({
