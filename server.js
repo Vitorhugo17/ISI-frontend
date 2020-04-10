5609d0efb0c6d98987075876ca1c666f927e5294
Vitorhugo17 <martinsvitor11@gmail.com>
2020-04-10 13:30:44 +0100

    alterações

--- server.js
+++ server.js
@@ -18,55 +18,74 @@ app.use(validator());
 const dirName = __dirname + '/template/code/';
 app.use(express.static(dirName));
 
+const urlBase = `http://localhost:8080`;
+
 app.set('view engine', 'ejs');
 app.set('views', 'template/code');
 
 app.get("/", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}login`);
+    response.render(`${dirName}login`, {
+        urlBase: urlBase
+    });
 })
 
 app.get("/register", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}registo`);
+    response.render(`${dirName}registo`, {
+        urlBase: urlBase
+    });
 })
 
 app.get("/recover", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}recuperar`);
+    response.render(`${dirName}recuperar`, {
+        urlBase: urlBase
+    });
 })
 
 app.get("/recoverPass/:id", (request, response) => {
     const user_id = request.sanitize("id").escape();
     response.set('Content-Type', 'text/html');
     response.render(`${dirName}alterarPass`, {
+        urlBase: urlBase,
         user_id: user_id
     });
 })
 
 app.get("/purchase", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}comprarBilhete`);
+    response.render(`${dirName}comprarBilhete`, {
+        urlBase: urlBase
+    });
 })
 
 app.get("/profile", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}perfil`);
+    response.render(`${dirName}perfil`, {
+        urlBase: urlBase
+    });
 })
 
 app.get("/unusedTickets", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}bilhetesNaoUtilizados`);    
+    response.render(`${dirName}bilhetesNaoUtilizados`, {
+        urlBase: urlBase
+    });
 })
 
 app.get("/usedTickets", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}bilhetesUtilizados`);    
+    response.render(`${dirName}bilhetesUtilizados`, {
+        urlBase: urlBase
+    });
 })
 
 app.get("/busSchedules", (request, response) => {
     response.set('Content-Type', 'text/html');
-    response.render(`${dirName}horarios`);    
+    response.render(`${dirName}horarios`, {
+        urlBase: urlBase
+    });
 })
 
 app.listen(process.env.PORT, () => console.log(`Node server listening on port ${process.env.PORT}!`));
