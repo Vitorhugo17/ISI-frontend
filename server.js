071f4942ee7334a8f045d9061d5e10809e9ef625
Vitorhugo17 <martinsvitor11@gmail.com>
2020-04-14 16:38:58 +0100

    alterações

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(validator());
 const dirName = __dirname + '/template/code/';
 app.use(express.static(dirName));
 
-const urlBase = `http://locahost:8080`;
+const urlBase = `http://localhost:8080`;
 
 app.set('view engine', 'ejs');
 app.set('views', 'template/code');
