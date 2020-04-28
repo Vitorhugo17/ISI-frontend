2e66db60369e3ed8f1aada6a90f1bb1dd4e0e85b
Vitorhugo17 <martinsvitor11@gmail.com>
2020-04-28 20:34:43 +0100

    alt

--- template/code/scripts/payment.js
+++ template/code/scripts/payment.js
@@ -1,7 +1,6 @@
 // A reference to Stripe.js
 let stripe;
 let paymentIntent;
-createPaymentIntent();
 
 // Disable the button until we have Stripe set up on the page
 document.querySelector("button").disabled = true;
@@ -10,8 +9,58 @@ fetch(`${urlBase}/stripe-key`, {
     credentials: 'include'
 }).then(function (result) {
     return result.json();
-}).then(function (data) {
+}).then(async function (data) {
     stripe = Stripe(data.publishableKey);
+    document.querySelector("button").disabled = false;
+
+    document.getElementById("formCompraBilhetes").addEventListener("submit", (event) => {
+        event.preventDefault();
+        pay();
+    });
+});
+
+async function pay() {
+    await createPaymentIntent();
+    const paymentRequest = stripe.paymentRequest({
+        country: 'PT',
+        currency: 'eur',
+        total: {
+            label: 'Total',
+            amount: parseInt((parseFloat(document.getElementById('preco').value.replace(",", ".")) * 100).toFixed(0)),
+        },
+        requestPayerName: true
+    });
+
+    // Callback when a payment method is created.
+    paymentRequest.on('paymentmethod', async event => {
+        // Confirm the PaymentIntent with the payment method returned from the payment request.
+        console.log(1);
+        const {
+            error
+        } = await stripe.confirmCardPayment(
+            paymentIntent.client_secret, {
+                payment_method: event.paymentMethod.id,
+            }, {
+                handleActions: false
+            }
+        );
+        if (error) {
+            // Report to the browser that the payment failed.
+            event.complete('fail');
+            handlePayment({
+                error
+            });
+        } else {
+            // Report to the browser that the confirmation was successful, prompting
+            // it to close the browser payment method collection interface.
+            event.complete('success');
+            // Let Stripe.js handle the rest of the payment flow, including 3D Secure if needed.
+            const response = await stripe.confirmCardPayment(
+                paymentIntent.client_secret
+            );
+            handlePayment(response);
+        }
+    });
 
     if (document.getElementById('referencia').className == "checkbox radio-two radio-two-checked") {
         const sourceData = {
@@ -20,6 +69,7 @@ fetch(`${urlBase}/stripe-key`, {
             currency: paymentIntent.currency,
             owner: {
                 name: document.getElementById("nomeUtilizador").value,
+                email: `isi+fill_now@gmail.com`
             },
             statement_descriptor: 'Stripe Payments Demo',
             metadata: {
@@ -34,49 +84,47 @@ fetch(`${urlBase}/stripe-key`, {
         document.querySelector("button").disabled = false;
         let elements = stripe.elements();
         let style = {
-        base: {
-            color: "#32325d",
-            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-            fontSmoothing: "antialiased",
-            fontSize: "16px",
-            "::placeholder": {
-            color: "#aab7c4"
+            base: {
+                color: "#32325d",
+                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                fontSmoothing: "antialiased",
+                fontSize: "16px",
+                "::placeholder": {
+                    color: "#aab7c4"
+                }
+            },
+            invalid: {
+                color: "#fa755a",
+                iconColor: "#fa755a"
             }
-        },
-        invalid: {
-            color: "#fa755a",
-            iconColor: "#fa755a"
-        }
         };
-    
+
         let card = elements.create("card", {
-        hidePostalCode: true,
-        style: style
+            hidePostalCode: true,
+            style: style
         });
         card.mount("#card-element");
         let form = document.getElementById("payment-form");
-        form.addEventListener("submit", function (event) {
+        form.addEventListener("submit", async function (event) {
             event.preventDefault();
 
             const response = await stripe.confirmCardPayment(
-                paymentIntent.client_secret,
-                {
-                  payment_method: {
-                    card,
-                    billing_details: {
-                      name,
-                    },
-                  },
-                  shipping,
+                paymentIntent.client_secret, {
+                    payment_method: {
+                        card,
+                        billing_details: {
+                            name: document.getElementById("nomeUtilizador").value,
+                        },
+                    }
                 }
-              );
-              handlePayment(response);
+            );
+            handlePayment(response);
         });
     }
-});
+}
 
 // Create the PaymentIntent with the cart details.
-async function createPaymentIntent() {
+async function createPaymentIntent(payment_method) {
     try {
         let tipoBilhete = document.getElementById("tipoBilhete").options[document.getElementById("tipoBilhete").selectedIndex].value;
         let res = tipoBilhete.split("-");
@@ -84,7 +132,8 @@ async function createPaymentIntent() {
         let orderData = {
             product_id: res[0],
             company: res[1],
-            quantity: document.getElementById('quantidade').value
+            quantity: document.getElementById('quantidade').value,
+            payment_method: payment_method
         }
         const response = await fetch(`${urlBase}/payment`, {
             method: 'POST',
@@ -95,7 +144,7 @@ async function createPaymentIntent() {
         });
         const data = await response.json();
         if (data.error) {
-           throw new Error(data.error);
+            throw new Error(data.error);
         } else {
             paymentIntent = data.paymentIntent;
         }
@@ -107,6 +156,8 @@ async function createPaymentIntent() {
             showConfirmButton: false,
             focusConfirm: false,
             timer: 2000
+        }).then(result => {
+            window.location.reload();
         })
     }
 }
@@ -123,7 +174,7 @@ function handleSourceActiviation(source) {
                 <p style="line-height: 27px; font-size: 15px; text-align: center; margin-bottom: 20px;">
                     <strong>Entidade: </strong>${multibanco.entity} <br>
                     <strong>Referência: </strong>${multibanco.reference} <br>
-                    <strong>Valor: </strong>${source.amount} €
+                    <strong>Valor: </strong>${((source.amount/100).toFixed(2)).replace(".", ",")} €
                 </p>
             </div>
             <br>
@@ -141,9 +192,10 @@ function handleSourceActiviation(source) {
 };
 
 async function pollPaymentIntentStatus(paymentIntent, start) {
-    let timeout = 30000;
+    let timeout = 3000000;
     let interval = 500;
     start = start ? start : new Date();
+    console.log(start, new Date());
     const endStates = ['succeeded', 'processing', 'canceled'];
     // Retrieve the PaymentIntent status from our server.
     const rawResponse = await fetch(`${urlBase}/payment/${paymentIntent}/status`, {
@@ -157,6 +209,7 @@ async function pollPaymentIntentStatus(paymentIntent, start) {
             interval
         );
     } else {
+        console.log(response);
         handlePayment(response);
         if (!endStates.includes(response.paymentIntent.status)) {
             // Status has not changed yet. Let's time out.
@@ -172,6 +225,7 @@ function handlePayment(paymentResponse) {
     } = paymentResponse;
 
     if (error) {
+        console.log(error);
         swal({
             title: 'Ocorreu um erro! Tente novamente mais tarde',
             type: 'error',
@@ -209,39 +263,3 @@ function handlePayment(paymentResponse) {
         })
     }
 }
-
-const paymentRequest = stripe.paymentRequest({
-    country: 'PT',
-    currency: 'eur',
-    total: {
-        label: 'Total',
-        amount: document.getElementById('preco').value,
-    },
-    requestPayerName: true
-});
-
-  // Callback when a payment method is created.
-paymentRequest.on('paymentmethod', async event => {
-    // Confirm the PaymentIntent with the payment method returned from the payment request.
-    const {error} = await stripe.confirmCardPayment(
-      paymentIntent.client_secret,
-      {
-        payment_method: event.paymentMethod.id,
-      },
-      {handleActions: false}
-    );
-    if (error) {
-      // Report to the browser that the payment failed.
-      event.complete('fail');
-      handlePayment({error});
-    } else {
-      // Report to the browser that the confirmation was successful, prompting
-      // it to close the browser payment method collection interface.
-      event.complete('success');
-      // Let Stripe.js handle the rest of the payment flow, including 3D Secure if needed.
-      const response = await stripe.confirmCardPayment(
-        paymentIntent.client_secret
-      );
-      handlePayment(response);
-    }
-  });
